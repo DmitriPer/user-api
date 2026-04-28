983574da5c1457b5cfce7966daf352dd6c24d9e7
Dimitri.P <Dimitri.pereimak@gmail.com>
2026-04-28 14:53:47 +0300

    refactor: split into routes, controllers, types

---
+++ src/mock/users.mock.ts
@@ -0,0 +1,13 @@
+import { User } from "../types/user.types";
+export const USERS: User[] = [
+    {
+        id: 1,
+        name: 'John Doe',
+        email: 'john@example.com'
+    },
+    {
+        id: 2,
+        name: 'Jane Smith',
+        email: 'jane@example.com'
+    }
+];
